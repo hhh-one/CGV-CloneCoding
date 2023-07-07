640cd128af92422308fe58823c1ca477702ff90c
hhh-one <904lhw@naver.com>
2023-07-07 11:41:44 +0900

    feat: event

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.swiper', {
+// 무비차트 swiper
+const swiper = new Swiper('.swiper.movieChart-list', {
     slidesPerView: 5,
     slidesPerGroup: 5,
     spaceBetween: 30,
@@ -10,3 +11,38 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     }
 });
+
+// event swiper
+const eventSwiper = new Swiper('.swiper.event-list', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 32,
+    alllowSlideNext: false,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 3000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
+
+// event swiper 자동 멈추기 / 재생하기
+var btnEventControl = document.querySelector('.btn-event-control');
+btnEventControl.onclick = function() {
+    if (btnEventControl.classList.contains('active')) {
+        btnEventControl.classList.remove('active');
+        eventSwiper.autoplay.start();
+    } else {
+        btnEventControl.classList.add('active');
+        eventSwiper.autoplay.stop();
+
+    }
+}
+
+//go to top
+var btnGoToTop = document.querySelector('.btn-gotoTop');
+btnGoToTop.onclick = function() {
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
